fc2b63b35fd3cc5cf654bcb3fecf0075e694c7fe
matthewromer <mromer60@gmail.com>
2021-05-04 19:48:05 -0600

    Intermediate

--- Project/First_Script.m
+++ Project/First_Script.m
@@ -12,47 +12,58 @@ p = size(C,1);                   %Number of inputs/outputs in agent s-s
 
 N = 2;                           %Number of nodes in network
 
-% Gamma = [0 1 0;
-%          1 0 1;
-%          0 1 0]; 
-% Deg =   [1  0  0;
-%          0  2  0;
-%          0  0  1];
-% Lap = Deg-Gamma;
-
+%Define graph. Two-agent toy system 
 Gamma = [0 1;
          1 0];
 Deg   = [1 0;
          0 1];
 Lap = Deg-Gamma;     
      
-ss_single = ss(A,B,C,0);   
-QCtl = eye(n)*10;
-RCtl = 1;
-[K, ~, ~] = lqr(ss_single,QCtl,RCtl);
+%Place controller and observer poles
 K = place(A,B,[-1,-2]);
-
 Lt = place(A',C',[-0.01 -0.02]);
 L = Lt';
 
+
+%Lt = place(A',C',[-0.1 -2]);
+%L = Lt';
+
+%Compute eigenvalues of controller and observer individually 
+eig_obs = eig(kron(eye(N),A-L*C))
+eig_control = eig(kron(eye(N),A)-kron(Lap,B*K))
+
 %Create full system dynamcis matrices 
 Atilde = [kron(eye(N),A-L*C), kron(Gamma,L*C);
-          kron(eye(N),A) + kron(Deg,B*K), kron(eye(N),-B*K)];
+          kron(eye(N),B*K),kron(eye(n),A)-kron(Deg,B*K)];
 Btilde = zeros(N*n*2,1);
 Ctilde = [kron(eye(N),zeros(size(C))),kron(eye(N),C)];
 
-ss_1 = ss(Atilde,Btilde,Ctilde,0);
-
-eig_obs = eig(kron(eye(N),A-L*C))
-eig_plant = eig(kron(eye(N),A)-kron(Lap,B*K))
-
+%Compute eigenvalues of full system A matrix 
 [V,Diag] = eig(Atilde);
+diag(Diag)
 V
-eig(Diag)
 
-x_0 = [0.1 0 0.2 0];
+Kval = 1;
 
-ss_plant_only = ss(kron(eye(N),A)-kron(Lap,B*K),zeros(4,1),[1 0 0 0;0 0 1 0],0);
 
-A_tilde_no_obs = (kron(eye(N),A)-kron(Lap,B*K))
-%[V,Diag] = eig(A_tilde_no_obs)
+
+
+%Define set of leaders
+G = zeros(N,Kval); 
+    
+%Create G, First m agents are leaders
+for i = 1:Kval
+   G(i,i) = 1;
+end
+
+Btilde = kron(G,B);
+
+rank(ctrb(kron(eye(N),A)-kron(Lap,B*C),Btilde))
+
+
+G = [1; 0];
+Btilde = [kron(G,zeros(size(B)));kron(G,B)];
+
+
+rank(ctrb(Atilde,Btilde))
+ 
